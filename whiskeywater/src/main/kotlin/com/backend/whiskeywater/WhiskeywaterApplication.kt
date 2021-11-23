df4f5919a7009d8cab57c20f1cdc0dbca9907632
unknown <sonykarl14@gmail.com>
2021-11-23 09:53:15 +0300

    initial repository

---
+++ whiskeywater/src/main/kotlin/com/backend/whiskeywater/WhiskeywaterApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.whiskeywater
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhiskeywaterApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhiskeywaterApplication>(*args)
+}
---
+++ whiskeywater/src/test/kotlin/com/backend/whiskeywater/WhiskeywaterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.whiskeywater
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhiskeywaterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
