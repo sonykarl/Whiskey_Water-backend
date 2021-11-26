201c4252f78303281b5996bd76a826b66ae2193b
unknown <sonykarl14@gmail.com>
2021-11-26 21:34:06 +0300

    added services

--- whiskeywater/src/main/kotlin/com/backend/whiskeywater/Customer/CustomerServices.kt
+++ whiskeywater/src/main/kotlin/com/backend/whiskeywater/Customer/CustomerServices.kt
@@ -1,4 +1,13 @@
 package com.backend.whiskeywater.Customer
 
-class CustomerServices {
+import org.springframework.beans.factory.annotation.Autowired
+
+class CustomerServices  @Autowired constructor(val customerRepository: CustomerRepository){
+    fun save(customer: Customer){
+        customerRepository.save(customer)
+    }
+
+    fun findByEmail(email:String):Customer?{
+        return customerRepository.findByEmail(email)
+    }
 }
