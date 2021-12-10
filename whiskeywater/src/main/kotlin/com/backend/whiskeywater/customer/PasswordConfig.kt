43ed5bafcc96ed75ef5f224830d537dc5895d9aa
unknown <sonykarl14@gmail.com>
2021-12-10 23:12:31 +0300

    secured endpoints

--- whiskeywater/src/main/kotlin/com/backend/whiskeywater/customer/PasswordConfig.kt
+++ whiskeywater/src/main/kotlin/com/backend/whiskeywater/customer/PasswordConfig.kt
@@ -1,10 +1,16 @@
 package com.backend.whiskeywater.customer
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+
 
 @Configuration
-@EnableWebSecurity
-class PasswordConfig: WebSecurityConfigurerAdapter(){
+class PasswordConfig{
+
+    @Bean
+    fun passwordEncoder():BCryptPasswordEncoder{
+        return BCryptPasswordEncoder()
+    }
 }
--- whiskeywater/src/main/kotlin/com/backend/whiskeywater/customer/Websecurityconfig.kt
+++ whiskeywater/src/main/kotlin/com/backend/whiskeywater/customer/Websecurityconfig.kt
@@ -1,4 +1,37 @@
 package com.backend.whiskeywater.customer
 
-class Websecurityconfig {
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class Websecurityconfig @Autowired constructor(private val customerservice: CustomerService, val passwordConfig: PasswordConfig): WebSecurityConfigurerAdapter(){
+    override fun configure(http: HttpSecurity?) {
+        http
+            ?.csrf()?.disable()
+            ?.authorizeRequests()
+            ?.antMatchers("/api/v*/registration/**")
+            ?.permitAll()
+            ?.anyRequest()
+            ?.authenticated()?.and()
+            ?.formLogin()
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth
+            ?.authenticationProvider(authentication())
+    }
+    @Bean
+    fun authentication():DaoAuthenticationProvider{
+        val provider = DaoAuthenticationProvider()
+        provider.setPasswordEncoder(passwordConfig.passwordEncoder())
+        provider.setUserDetailsService(customerservice)
+        return provider
+    }
 }
