47289be3c9feb279cab235ae3f7dda289b4c08af
unknown <sonykarl14@gmail.com>
2021-12-15 04:50:42 +0300

    added models

--- whiskeywater/src/main/kotlin/com/backend/whiskeywater/Shopkeeper/ShopkeeperRepo.kt
+++
@@ -1,8 +0,0 @@
-package com.backend.whiskeywater.Shopkeeper
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.repository.CrudRepository
-
-interface ShopkeeperRepo:JpaRepository<Shopkeeper,Int> {
-    fun findByUsername(username:String?):Shopkeeper?
-}
---
+++ whiskeywater/src/main/kotlin/com/backend/whiskeywater/authentication/Customer.kt
@@ -0,0 +1,18 @@
+package com.backend.whiskeywater.authentication
+
+import javax.persistence.*
+
+@Entity
+data class Customer (
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long,
+    @Column(unique = true)
+    val email: String,
+    @Column
+    val firstName: String,
+    @Column
+    val lastName: String?,
+    @Column
+    val password: String
+        )
